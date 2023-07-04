72d0c6f5c87d76c02d0831a5adba697f96f86b9d
Reinaldo Alves <reinaldoasjr8@gmail.com>
2023-07-04 16:56:40 -0300

    responsive layout

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const contactLink = document.getElementById('contact-li');
 function scroll(pages) {
     const scrollPixels = window.innerHeight * pages;
     window.scrollTo({top: scrollPixels, behavior: 'smooth'})
-    console.log('executou')
 }
 
 homepageLink.addEventListener('click', () => scroll(0));
@@ -20,37 +19,47 @@ const cardsContainer = document.getElementById('cards-container');
 const arrowLeft = document.getElementById('arrow-left');
 const arrowRight = document.getElementById('arrow-right');
 
-arrowLeft.addEventListener('click', () => cardsContainer.scrollLeft -= 500)
-arrowRight.addEventListener('click', () => cardsContainer.scrollLeft += 500)
+function scrolltoLeft() {
+    const maxScroll = cardsContainer.scrollWidth - cardsContainer.clientWidth;
+    const pixScroll = window.innerWidth > 696 ? 500 : 300;
+    if (cardsContainer.scrollLeft === 0) {
+        cardsContainer.scrollLeft = maxScroll;
+    } else {
+        cardsContainer.scrollLeft -= pixScroll;
+    }
+}
 
-//PROJETOS
-//Reprodução da Página inicial do Facebook
-//Site Conhecendo o Brasil
-//Calculadora IMC
-//Portifolio Reinaldo Alves
-//Star Wars Encyclopedia
-//Youtube Clone
-//WhatsApp Clone
+function scrolltoRight() {
+    const maxScroll = cardsContainer.scrollWidth - cardsContainer.clientWidth;
+    const pixScroll = window.innerWidth > 696 ? 500 : 300;
+    if (cardsContainer.scrollLeft === maxScroll) {
+        cardsContainer.scrollLeft = 0;
+    } else {
+        cardsContainer.scrollLeft += pixScroll;
+    }
+}
 
-// const button = document.getElementById('button');
-// const nome = document.getElementById('name');
-// const peso = document.getElementById('weight');
-// const altura = document.getElementById('height');
-// const resultado = document.getElementById('result')
+arrowLeft.addEventListener('click', () => scrolltoLeft());
+arrowRight.addEventListener('click', () => scrolltoRight());
 
-// const getText = (imc) => {
-//     if(imc > 40) return 'com obesidade grau III'
-//     if(imc > 35) return 'com obesidade grau II'
-//     if(imc > 30) return 'com obesidade grau I'
-//     if(imc > 25) return 'levemente acima do peso'
-//     if(imc > 18.5) return 'no peso ideal'
-//     return 'abaixo do peso'
-// }
+const tabWeb = document.getElementById('tab-1');
+const tabApp = document.getElementById('tab-2');
+const tabsContent = document.getElementById('tabs-content');
 
-// const imcCalc = () => {
-//     if(!peso.value || !altura.value || !nome.value) return resultado.textContent = "Preencha todos os campos corretamente"
-//     const valorIMC = (+peso.value / (+altura.value * +altura.value)).toFixed(1);   //+ serve para transformar string em numero
-//     resultado.textContent = `${nome.value}, seu IMC é ${valorIMC} e você está ${getText(valorIMC)}`;
-// }
+const textWeb = 'Páginas web para as mais diversas finalidades, como lojas, profissionais autônomos (advogados, corretores, cabelereiros, etc) e artistas, além de landing pages para quem vende algum produto ou serviço na internet. Os websites são desenvolvidos em HTML/CSS ou em React, de acordo com a necessidade ou preferência do cliente';
+const textApp = 'Sistemas com funcionalidades mais complexas e integrados com bancos de dados, como sistemas de agendamento de serviços, de gestão de estoque, de cadastro de funcionários/clientes, entre outros. A interface dos sistemas é desenvolvida em React, enquanto que o servidor é desenvolvido em Node.js com banco de dados MariaDB ou MySQL';
 
-// button.addEventListener('click', imcCalc)
+function activeWeb() {
+    tabsContent.innerHTML = textWeb;
+    tabWeb.classList.add('active-tab');
+    tabApp.classList.remove('active-tab');
+}
+
+function activeApp() {
+    tabsContent.innerHTML = textApp
+    tabApp.classList.add('active-tab');
+    tabWeb.classList.remove('active-tab');
+}
+
+tabWeb.addEventListener('click', () => activeWeb());
+tabApp.addEventListener('click', () => activeApp());
